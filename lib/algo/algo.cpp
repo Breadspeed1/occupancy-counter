4a1dbaf7e43b2222744f86e6aae300ec090804aa
Breadspeed1 <53412269+Breadspeed1@users.noreply.github.com>
2025-03-18 17:35:29 -0400

    create algo file

--- lib/algo/algo.cpp
+++ lib/algo/algo.cpp
@@ -1,7 +1,1 @@
 #include "algo.h"
-
-DetectionAlgorithm::DetectionAlgorithm(float* largeData, int ldlen, float* tempData, int tdlen, DetectionSettings settings) {
-    this->settings = settings;
-    this->largeBuf = CircularBuffer(largeData, ldlen);
-    this->tempBuf = CircularBuffer(tempData, tdlen);
-}
