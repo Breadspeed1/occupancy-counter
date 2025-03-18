26a076761f85d096326071c3b55a1666d60943e2
Breadspeed1 <53412269+Breadspeed1@users.noreply.github.com>
2025-03-18 17:43:33 -0400

    method stubs

--- lib/algo/algo.cpp
+++ lib/algo/algo.cpp
@@ -1,1 +1,25 @@
 #include "algo.h"
+
+DetectionState DetectionAlgorithm::getCurrentState() {
+    return currentState;
+}
+
+int DetectionAlgorithm::getEntranceCount() {
+    return entranceCount;
+}
+
+int DetectionAlgorithm::getExitCount() {
+    return exitCount;
+}
+
+int DetectionAlgorithm::getCurrentOccupancy() {
+    return entranceCount - exitCount;
+}
+
+void DetectionAlgorithm::reset() {
+    throw "not implemented";
+}
+
+bool DetectionAlgorithm::push(float measurement) {
+    throw "not implemented";
+}
